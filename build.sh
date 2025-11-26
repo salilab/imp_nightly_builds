bfbc07bd8586f3252a2447418849cb5a4d9cad01
Ben Webb <benmwebb@gmail.com>
2025-11-25 20:29:39 -0800

    Get cereal-devel from EPEL 10.1
    We need cereal-devel to test the IMP-devel package,
    but it is not present in EPEL 10.0, only in 10.1,
    and Rocky is still at 10.0. Build a repo with a
    suitable backport, and pull that in to mock.

--- build.sh
+++ build.sh
@@ -423,7 +423,7 @@ END
     else
       run_mock_build pkg.el8-x86_64 epel-8-x86_64 "-std=c++11" "" "" "" "mpi/mpich-x86_64"
       run_mock_build pkg.el9-x86_64 epel-9-x86_64 "" "" "" "" "mpi/mpich-x86_64"
-      run_mock_build pkg.el10-x86_64 epel-10-x86_64 "" "" "" "" "mpi/mpich-x86_64"
+      run_mock_build pkg.el10-x86_64 epel-10-x86_64 "" "" "" "--enablerepo=cereal-epel" "mpi/mpich-x86_64"
     fi
     rm -f config.py.$$
 
